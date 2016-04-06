7a97dad8a0410e64693667ff2bd64bc03403f4d7
smusaran <marcgomez@iesmontsia.org>
2016-04-06 19:45:15 +0200

    marc es negre?

--- admin/index.php
+++ admin/index.php
@@ -1,27 +1,49 @@
 <!-- Header content box -->
 <?php 
 $title='AdminPanel';
-$migas='#Inici|../index.php#Admin Panel|AdminPanel.php';
+$migas='#Inici|../index.php#Admin Panel|index.php';
 include "../Public/layouts/menu.php";?>
-
-<!-- Content body -->
-<!-- Body box -->
-<div class="body-box">
-    <!-- Menu Box -->
-    <div class="menu-box">
-        menu box
-    </div>
-
+    <script src="script.js"></script>
+    
+    
     <!-- Navigation Box -->
-    <div class="nav-box">
-        nav box
+    <div class="body-box">
+        <div class="nav-box">
+            <div id='cssmenu'>
+                <ul>
+                   <li><a href='#'><span>Home</span></a></li>
+                   <li class='active has-sub'><a href='#'><span>Products</span></a>
+                      <ul>
+                         <li class='has-sub'><a href='#'><span>Product 1</span></a>
+                            <ul>
+                               <li><a href='#'><span>Sub Product</span></a></li>
+                               <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                            </ul>
+                         </li>
+                         <li class='has-sub'><a href='#'><span>Product 2</span></a>
+                            <ul>
+                               <li><a href='#'><span>Sub Product</span></a></li>
+                               <li class='last'><a href='#'><span>Sub Product</span></a></li>
+                            </ul>
+                         </li>
+                      </ul>
+                   </li>
+                   <li><a href='#'><span>About</span></a></li>
+                   <li class='last'><a href='#'><span>Contact</span></a></li>
+                   <li><a href='#'><span>Productes</span></a></li>
+                   <li><a href='#'><span>Usuaris</span></a></li>
+                   <li><a href='#'><span>Admins</span></a></li>
+                   <li><a href='#'><span>BBDD</span></a></li>
+                   <li><a href='#'><span>Permisos</span></a></li>
+                </ul>
+            </div>
+        </div>
+
+        <!--Content Box -->
+        <div class="content-box">
+            content box
+            <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        </div> 
     </div>
-
-    <!--Content Box -->
-    <div class="content-box">
-        content box
-    </div> 
-</div>
-
 <!-- Footer content box -->
 <?php include "../Public/layouts/footer.php";?> 
