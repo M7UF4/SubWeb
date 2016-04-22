9ede04797c7a0602aedac7e860e7447d00ab94a7
smusaran <marcgomez@iesmontsia.org>
2016-04-22 16:25:09 +0200

    test

--- Public/layouts/menu.php
+++ Public/layouts/menu.php
@@ -13,7 +13,7 @@
             /* Css Jquery resources */
             $self = $_SERVER['PHP_SELF'];
             if (strpos($self,"admin/")) {
-                include "../System/Protocols/Guanyador.php";
+                include "../System/Protocols/test.php";
                 //include "../System/Errors.php"; //Debug Mode
                 echo '<LINK REL=StyleSheet HREF="../Public/css/font-awesome.css" TYPE="text/css" MEDIA=screen>';
                 echo '<LINK REL=StyleSheet HREF="../Public/css/style.css" TYPE="text/css" MEDIA=screen>';
@@ -24,7 +24,7 @@
                 echo '<script src="../Public/jquery/jquery-ui.min.js"></script>';
                 echo '<link rel="shortcut icon" href="../favicon.ico">';
             }else if (strpos($self,"user/")) {
-                include "../System/Protocols/Guanyador.php";
+                include "../System/Protocols/test.php";
                 //include "../System/Errors.php"; //Debug Mode
                 echo '<LINK REL=StyleSheet HREF="../Public/css/font-awesome.css" TYPE="text/css" MEDIA=screen>';
                 echo '<LINK REL=StyleSheet HREF="../Public/css/style.css" TYPE="text/css" MEDIA=screen>';
@@ -34,7 +34,7 @@
                 echo '<script src="../Public/jquery/jquery-ui.min.js"></script>';
                 echo '<link rel="shortcut icon" href="../favicon.ico">';
             }else{
-                include "System/Protocols/Guanyador.php";
+                include "System/Protocols/test.php";
                 //include "System/Errors.php"; //Debug Mode
                 
                 echo '<LINK REL=StyleSheet HREF="Public/css/font-awesome.css" TYPE="text/css" MEDIA=screen>';
